9135decc42fa7ed65f5d7c105c0e1fe83d312963
benthejack <benthejack@gmail.com>
2016-07-18 13:59:48 +1200

    step through functionality and button added

--- sketch.js
+++ sketch.js
@@ -4,16 +4,16 @@
 
 //feel free to experiment with changing these
 var CEM_settings = {
-	polygon_sides: 6 //hexagon - change this to 4 to see process as square, or 3 to see the process as a triangle
-}
+	polygon_sides: 6 //hexagon - change this to 4 to see the process for a square, or 3 to see the process for a triangle
+};
 
-//although you should feel free to play with these, they aren't general settings.
-var CEM_globals{
+//although you should feel free to play with these, they aren't general settings intended for changing.
+var CEM_globals = {
 	step_num: 0
-}
+};
 
 //steps is an array of functions, these functions will be called one at a time as the next button is pressed
-var steps = [
+CEM_globals.steps = [
 	circle,
 	angles,
 	polygon,
@@ -21,39 +21,50 @@ var steps = [
 	image_flip,
 	image_rotate,
 	tile
-]
+];
 
 function circle(){
-
+	console.log("circle");
 }
 
 function angles(){
-
+	console.log("angles");
 }
 
 function polygon(){
-
+	console.log("polygon");
 }
 
 function image_mask(){
-
+	console.log("image_mask");
 }
 
 function image_flip(){
-
+	console.log("image_flip");
 }
 
 function image_rotate(){
-
+	console.log("image_rotate");
 }
 
 function tile(){
+	console.log("tile");
+}
 
+function next_step(){
+	if(CEM_globals.step_num < CEM_globals.steps.length){
+		current_step = CEM_globals.steps[CEM_globals.step_num]
+		current_step();
+		CEM_globals.step_num++;
+	}
 }
 
 
 function setup() {
-	size(500,500);
+	createCanvas(500,500);
+	button = createButton('next step');
+	button.mousePressed(next_step);
+
 }
 
 function draw() {
